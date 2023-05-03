e634956cdbf85c6c480c8ae3cfc80d5da6fbb9fa
Aijaz <35755621+aijazafzaal@users.noreply.github.com>
2023-05-03 04:15:40 +0200

    Update transform.py

--- Mage-files/transform.py
+++ Mage-files/transform.py
@@ -24,6 +24,7 @@ def transform(df, *args, **kwargs):
     df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
     df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
 
+    
     datetime_dim = df[['tpep_pickup_datetime','tpep_dropoff_datetime']].drop_duplicates().reset_index(drop=True)
     datetime_dim['pick_hour'] = datetime_dim['tpep_pickup_datetime'].dt.hour
     datetime_dim['pick_day'] = datetime_dim['tpep_pickup_datetime'].dt.day
@@ -36,11 +37,11 @@ def transform(df, *args, **kwargs):
     datetime_dim['drop_month'] = datetime_dim['tpep_dropoff_datetime'].dt.month
     datetime_dim['drop_year'] = datetime_dim['tpep_dropoff_datetime'].dt.year
     datetime_dim['drop_weekday'] = datetime_dim['tpep_dropoff_datetime'].dt.weekday
-
+    
     datetime_dim['datetime_id'] = datetime_dim.index
     datetime_dim = datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
                              'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
-
+    
     passenger_count_dim = df[['passenger_count']].drop_duplicates().reset_index(drop=True)
     passenger_count_dim['passenger_count_id'] = passenger_count_dim.index
     passenger_count_dim = passenger_count_dim[['passenger_count_id','passenger_count']]
@@ -48,6 +49,7 @@ def transform(df, *args, **kwargs):
     trip_distance_dim = df[['trip_distance']].drop_duplicates().reset_index(drop=True)
     trip_distance_dim['trip_distance_id'] = trip_distance_dim.index
     trip_distance_dim = trip_distance_dim[['trip_distance_id','trip_distance']]
+
     rate_code_type = {
         1:"Standard rate",
         2:"JFK",
@@ -62,7 +64,6 @@ def transform(df, *args, **kwargs):
     rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type)
     rate_code_dim = rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']]
 
-
     pickup_location_dim = df[['pickup_longitude', 'pickup_latitude']].drop_duplicates().reset_index(drop=True)
     pickup_location_dim['pickup_location_id'] = pickup_location_dim.index
     pickup_location_dim = pickup_location_dim[['pickup_location_id','pickup_latitude','pickup_longitude']] 
@@ -71,6 +72,7 @@ def transform(df, *args, **kwargs):
     dropoff_location_dim = df[['dropoff_longitude', 'dropoff_latitude']].drop_duplicates().reset_index(drop=True)
     dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index
     dropoff_location_dim = dropoff_location_dim[['dropoff_location_id','dropoff_latitude','dropoff_longitude']]
+    
 
     payment_type_name = {
         1:"Credit card",
@@ -85,6 +87,7 @@ def transform(df, *args, **kwargs):
     payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name)
     payment_type_dim = payment_type_dim[['payment_type_id','payment_type','payment_type_name']]
 
+    
     fact_table = df.merge(passenger_count_dim, on='passenger_count') \
              .merge(trip_distance_dim, on='trip_distance') \
              .merge(rate_code_dim, on='RatecodeID') \
@@ -96,7 +99,8 @@ def transform(df, *args, **kwargs):
                'trip_distance_id', 'rate_code_id', 'store_and_fwd_flag', 'pickup_location_id', 'dropoff_location_id',
                'payment_type_id', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
                'improvement_surcharge', 'total_amount']]
-
+    
+    
     return {"datetime_dim":datetime_dim.to_dict(orient="dict"),
     "passenger_count_dim":passenger_count_dim.to_dict(orient="dict"),
     "trip_distance_dim":trip_distance_dim.to_dict(orient="dict"),
@@ -105,6 +109,9 @@ def transform(df, *args, **kwargs):
     "dropoff_location_dim":dropoff_location_dim.to_dict(orient="dict"),
     "payment_type_dim":payment_type_dim.to_dict(orient="dict"),
     "fact_table":fact_table.to_dict(orient="dict")}
+
+    print(fact_table)
+    return "success"
 
 
 @test
